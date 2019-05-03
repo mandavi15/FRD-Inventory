b09072965167a5e3d666f110f93ae735ad7e5e56
mandavi15 <er.mandavi@gmail.com>
2019-05-03 10:23:23 +0530

    Created pages

--- NeproFinishedGoodsAngular5/ClientApp/src/app/material-received-note/material-received-note.component.ts
+++ NeproFinishedGoodsAngular5/ClientApp/src/app/material-received-note/material-received-note.component.ts
@@ -35,20 +35,19 @@ export class MaterialReceivedNoteComponent {
     { vendorCode: '002', vendorname: 'Fateh', PON: 'PON-02', status: 'Active' },
     { vendorCode: '003', vendorname: 'Fateh', PON: 'PON-03', status: 'Active' },
     { vendorCode: '004', vendorname: 'Fateh', PON: 'PON-04', status: 'Active' },
-    { vendorCode: '005', vendorname: 'Fateh', PON: 'PON-05', status: 'Active' },
-    { vendorCode: '006', vendorname: 'Fateh', PON: 'PON-06', status: 'Active' }
+    { vendorCode: '005', vendorname: 'Fateh', PON: 'PON-05', status: 'Active' }
   ];
 
   columnDefs1 = [
-    { headerName: 'Item No.', field: 'itemno', sortable: true, filter: true, width: 90 },
-    { headerName: 'Item Name', field: 'productname', sortable: true, filter: true, width: 100 },
-    { headerName: 'Item Name(A)', field: 'productnameArabic', sortable: true, filter: true, width: 125 },
+    { headerName: 'Item No.', field: 'itemno', sortable: true, filter: true, width: 84 },
+    { headerName: 'Item Name', field: 'productname', sortable: true, filter: true, width: 96 },
+    { headerName: 'Item Name(A)', field: 'productnameArabic', sortable: true, filter: true, width: 112 },
     { headerName: 'Configuration', field: 'configuration', sortable: true, filter: true, width: 110 },
-    { headerName: 'Remaining Qty', field: 'remQty', sortable: true, filter: true, width: 120 },
+    { headerName: 'Remaining Qty', field: 'remQty', sortable: true, filter: true, width: 117 },
     { headerName: 'Unit', field: 'unit', sortable: true, filter: true, width: 70 },
-    { headerName: 'Received Qty', field: 'receivedQty', sortable: true, filter: true, width: 140 },
+    { headerName: 'Received Qty', field: 'receivedQty', sortable: true, filter: true, width: 110 },
     { headerName: 'Created Batch Qty', field: 'batchQty', sortable: true, filter: true, width: 140 },
-    { headerName: '', field: '', width: 40 }
+    { headerName: '', field: '', template: `<button class="btn pull-right btn-success btn-sm">Create Batch</button>`, width: 100 }
   ];
 
   rowData1 = [
@@ -57,8 +56,8 @@ export class MaterialReceivedNoteComponent {
     { itemno: '3', productname: 'Rice', productnameArabic: 'qqqqq', configuration: 'Config 1', remQty: '100', unit: 'kg', receivedQty: '50', batchQty: '30' },
     { itemno: '4', productname: 'Rice', productnameArabic: 'qqqqq', configuration: 'Config 1', remQty: '100', unit: 'kg', receivedQty: '50', batchQty: '30' },
     { itemno: '5', productname: 'Rice', productnameArabic: 'qqqqq', configuration: 'Config 1', remQty: '100', unit: 'kg', receivedQty: '50', batchQty: '30' },
-    { itemno: '6', productname: 'Rice', productnameArabic: 'qqqqq', configuration: 'Config 1', remQty: '100', unit: 'kg', receivedQty: '50', batchQty: '30' }
-  ];
+
+    ];
   
   columnDefs2 = [
     { headerName: 'Configuration', field: 'config', sortable: true, filter: true, width: 110 },
@@ -73,9 +72,28 @@ export class MaterialReceivedNoteComponent {
     { config: 'Config 2', batchNo: 'Batch 02', expiryDate: '15/05/2019', qty: '100' },
     { config: 'Config 3', batchNo: 'Batch 03', expiryDate: '15/05/2019', qty: '70' },
     { config: 'Config 4', batchNo: 'Batch 04', expiryDate: '15/05/2019', qty: '50' },
-    { config: 'Config 5', batchNo: 'Batch 05', expiryDate: '15/05/2019', qty: '80' },
-    { config: 'Config 6', batchNo: 'Batch 06', expiryDate: '15/05/2019', qty: '60' }
+    { config: 'Config 5', batchNo: 'Batch 05', expiryDate: '15/05/2019', qty: '80' }
 
+  ];
+
+ columnDefs3 = [
+   { headerName: 'PO No.', field: 'PON', sortable: true, filter: true, width: 90 },
+   { headerName: 'Vendor Name', field: 'vendorname', sortable: true, filter: true, width: 130 },
+   { headerName: 'MRN No.', field: 'MRNno', sortable: true, filter: true, width: 125 },
+   { headerName: 'Activity No.', field: 'activityNo', sortable: true, filter: true, width: 110 },
+   { headerName: 'MRN Date', field: 'MRNDate', sortable: true, filter: true, width: 120 },
+   { headerName: 'Activity Date', field: 'activityDate', sortable: true, filter: true, width: 130 },
+   { headerName: 'Received Qty', field: 'receivedQty', sortable: true, filter: true, width: 130 },
+   { headerName: '', field: '', width: 110 }
+  
+  ];
+
+  rowData3 = [
+    { PON: 'PO-1', vendorname: 'Fateh', MRNno: 'MRN-01', activityNo: 'Act 1', MRNDate: '02/05/2019', activityDate: '02/05/2019', receivedQty: '50' },
+    { PON: 'PO-1', vendorname: 'Fateh', MRNno: 'MRN-01', activityNo: 'Act 1', MRNDate: '02/05/2019', activityDate: '02/05/2019', receivedQty: '50' },
+    { PON: 'PO-1', vendorname: 'Fateh', MRNno: 'MRN-01', activityNo: 'Act 1', MRNDate: '02/05/2019', activityDate: '02/05/2019', receivedQty: '50' },
+    { PON: 'PO-1', vendorname: 'Fateh', MRNno: 'MRN-01', activityNo: 'Act 1', MRNDate: '02/05/2019', activityDate: '02/05/2019', receivedQty: '50' },
+    { PON: 'PO-1', vendorname: 'Fateh', MRNno: 'MRN-01', activityNo: 'Act 1', MRNDate: '02/05/2019', activityDate: '02/05/2019', receivedQty: '50' }
   ];
 
  }
